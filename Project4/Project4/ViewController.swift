1248714a6a542886eda6cf5451f6a94afdbf542f
Christian Mitteldorf <cmit@danskebank.dk>
2019-06-03 14:01:03 +0200

    Add empty Project 4

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Christian Mitteldorf on 03/06/2019.
+//  Copyright © 2019 MobilePay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
