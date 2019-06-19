2b0f1a76f7d5829fe5cbcb8077b785c18b290803
Christian Mitteldorf <christian.km@me.com>
2019-06-19 08:52:49 +0200

    Create project 7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Christian Mitteldorf on 18/06/2019.
+//  Copyright © 2019 Christian Mitteldorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
