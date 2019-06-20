1bcbd367b78a2357b81a15ff131f511042eb0f16
Christian Mitteldorf <cmit@danskebank.dk>
2019-06-20 17:35:55 +0200

    Complete day 34

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -12,9 +12,44 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+
+
+
+
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            } else {
+                showError()
+            }
+        } else {
+            showError()
+        }
+    }
+
+    func showError() {
+        let alertController = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alertController, animated: true)
     }
 
 
 }
 
+extension ViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
