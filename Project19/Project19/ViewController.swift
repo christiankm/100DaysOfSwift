39268416d7f084774616653da325a833f936701e
Christian Mitteldorf <christian.km@me.com>
2019-07-20 12:50:29 +0200

    Partly complete Project 19

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Christian Mitteldorf on 20/07/2019.
+//  Copyright © 2019 Christian Mitteldorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
